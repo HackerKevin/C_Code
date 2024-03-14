00c3de715a9280b8e4c5fe93cb087f53dadfc8ad
HackerKevin <138192787+HackerKevin@users.noreply.github.com>
2024-03-14 20:49:00 +0800

    Add files via upload

---
+++ test_2024_0313/RBTree.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "Assistance.h"
+
+enum Colour
+{
+	RED,
+	BLACK,
+};
+
+template<class K, class V>
+struct RBTreeNode
+{
+	RBTreeNode<K, V>* _left;
+	RBTreeNode<K, V>* _right;
+	RBTreeNode<K, V>* _parent;
+	pair<K, V> _kv;
+	Colour _col;// 颜色
+
+	RBTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _kv(kv)
+		, _col(RED)
+	{}
+};
+
+template<class K, class V>
+class RBTree
+{
+	typedef RBTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			_root->col = BLACK;// 根节点的颜色为黑色
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first > kv.first)
+		{
+			parent->_left = cur;
+		}
+		else
+		{
+			parent->_right = cur;
+		}
+
+		cur->_parent = parent;// 反向链接父节点
+
+		// 每次新增的节点是红色，因此需要
+		return true;
+	}
+private:
+	Node* _root;
+};
